7291f16e185679753ae180f7cfe0094cf756d1a9
sins <sinsmailza@gmail.com>
2025-07-20 23:27:22 +0200

    feat: refactor login form and integrate user fetching
    - Created a new LoginForm component to handle user input for name, age, and email
    - Integrated form validation using react-hook-form and Zod
    - Updated the homepage to fetch the first user and pass data to the LoginForm
    - Added a function to retrieve the first user from the database

--- shadcn_forms/src/app/page.tsx
+++ shadcn_forms/src/app/page.tsx
@@ -1,83 +1,10 @@
-"use client";
+import { getFirstUser } from "@/lib/actions";
+import LoginForm from "./login-form";
 
-import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import createUser from "@/lib/actions";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { FormSchemaType, formSchema } from "@/lib/zod";
-
-export default function LoginForm() {
-  const form = useForm<FormSchemaType>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      name: "",
-      age: "0",
-      email: ""
-    }
-  })
-
-  async function onSubmit(values: FormSchemaType) {
-    await createUser(values)
-    form.reset()
-  }
-
+export default async function Home() {
+  const firstUser = await getFirstUser();
+  
   return (
-    <div className="w-md mx-auto flex flex-col gap-4 p-4">
-      {/* 👇 CENTRALIZED ERROR BLOCK */}
-      {Object.entries(form.formState.errors).length > 0 && (
-        <div className="rounded-md border border-destructive bg-destructive/10 p-4 text-destructive text-sm space-y-1">
-          {Object.entries(form.formState.errors).map(([fieldName, error]) => (
-            <p key={fieldName}>
-              {fieldName}: {error?.message?.toString()}
-            </p>
-          ))}
-        </div>
-      )}
-      <Form {...form} >
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Name</FormLabel>
-                <FormControl>
-                  <Input placeholder="name" {...field} />
-                </FormControl>
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="age"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Age</FormLabel>
-                <FormControl>
-                  <Input placeholder="age" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input placeholder="email" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button className="w-full" type="submit">Submit</Button>
-        </form>
-      </Form>
-    </div>
-  )
+    <LoginForm {...firstUser[0]} />
+  );
 }
--- shadcn_forms/src/lib/actions.ts
+++ shadcn_forms/src/lib/actions.ts
@@ -4,10 +4,15 @@ import { db } from "./index";
 import { usersTable } from "@/db/schema";
 import { FormSchemaType } from "./zod";
 
-export default async function createUser(values: FormSchemaType) {
+export async function createUser(values: FormSchemaType) {
   await db.insert(usersTable).values({
     name: values.name,
     age: parseInt(values.age),
     email: values.email
   })
+}
+
+export async function getFirstUser() {
+  const firstUser = await db.select().from(usersTable).limit(1);
+  return firstUser;
 }
